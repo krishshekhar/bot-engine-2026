7ed202d411a9c74eca12bb00e6b8099c76521beb
krishshekhar <krishshekhar45@gmail.com>
2026-03-06 19:56:56 +0530

    pokerbot

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 PYTHON_CMD = ".venv312/bin/python"
 # For linux and mac, the code python cmd is sometimes 'python3' instead of 'python'
 
-BOT_1_NAME = 'bestbot'
-BOT_1_FILE = './bestbot.py'
+BOT_1_NAME = 'fuckmeintheasshole'
+BOT_1_FILE = './fuckmeintheasshole.py'
 
-BOT_2_NAME = 'bestbotv2.2'
-BOT_2_FILE = './bestbotv2.2.py'
+BOT_2_NAME = 'botharshu_v6_copy_4'
+BOT_2_FILE = './botharshu_v6_copy_4.py'
 
 # GAME PROGRESS IS RECORDED HERE
 GAME_LOG_FOLDER = './logs'
