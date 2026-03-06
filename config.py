c1b965650a33e77bdcef6acb210a6beb85c840c9
krishshekhar <krishshekhar45@gmail.com>
2026-03-06 18:32:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 PYTHON_CMD = ".venv312/bin/python"
 # For linux and mac, the code python cmd is sometimes 'python3' instead of 'python'
 
-BOT_1_NAME = 'bestbotv2'
-BOT_1_FILE = './bestbotv2.py'
+BOT_1_NAME = 'bestbot'
+BOT_1_FILE = './bestbot.py'
 
 BOT_2_NAME = 'bestbotv2.2'
 BOT_2_FILE = './bestbotv2.2.py'
